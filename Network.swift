9e94a2deb75572779462d3e8e89957e4025f6235
memphisdoit <47563597+memphisdoit@users.noreply.github.com>
2019-02-12 15:12:16 +0200

    Add files via upload

---
+++ Network.swift
@@ -0,0 +1,272 @@
+//
+//  Network.swift
+//  Memphis
+//
+//  Created by Igor Tudoran on 12/26/18.
+//  Copyright © 2018 Igor Tudoran. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+// MARK: - Request Protocol
+
+protocol RequestProtocol {
+    var baseURL: String { get }
+    var path: Path { get }
+    var fullURL: String { get }
+    var encoding: ParameterEncoding { get }
+    var method: HTTPMethod { get }
+    var headers: HTTPHeaders { get }
+    var parameters: Parameters { get }
+    var queryID: String? { get }
+}
+
+// MARK: - Network class
+
+@objc class Network: NSObject {
+    
+    // Shared istance to work with
+    @objc static let shared = Network()
+    
+    // Network's session & reachability managers
+    var sessionManager: SessionManager!
+    private var reachabilityManager: NetworkReachabilityManager! = NetworkReachabilityManager()
+    
+    // Current request for canceling or etc
+    var currentRequest: Request?
+    
+    // Reachability check
+    class var isReachable: Bool {
+        return NetworkReachabilityManager()!.isReachable
+    }
+    
+    // Initializing session & managers
+    func setup() {
+        let config = URLSessionConfiguration.default
+        sessionManager = SessionManager(configuration: config)
+        
+        // reachablity
+        reachabilityManager.listener = { status in
+            print("Network Status Changed: \(status)")
+            notificationCenter.post(name: kReachibilityChanged,
+                                    object: status)
+        }
+        reachabilityManager.startListening()
+        
+    }
+    
+    // Main request method to use universally
+    func request(_ request: APIRequest, completion: @escaping APIResponse) {
+        self.currentRequest = self.sessionManager.request(request.fullURL,
+                                                          method: request.method,
+                                                          parameters: request.parameters,
+                                                          encoding: request.encoding,
+                                                          headers: request.headers)
+            .validate()
+            .responseData { response in
+                // Status code checkings
+                self.currentRequest = nil
+                let statusCode = response.response?.statusCode ?? -1000
+                print("Status Code: ", statusCode)
+                
+                // Result maintaining
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    completion(json, nil)
+                    // clearing Network's current request
+                    self.currentRequest = nil
+                case .failure(let error):
+                    // if data hase error message than custom error is made with description
+                    if let data = response.data {
+                        let errJson = JSON(data)
+                        var errMessage = errJson["message"].stringValue
+                        if errMessage.isEmpty {
+                            errMessage = "Something went wrong, please try again later"
+                        }
+                        let err = NSError(domain: "network",
+                                code: statusCode,
+                                userInfo: [NSLocalizedDescriptionKey: errMessage])
+                        print("Error:", err)
+                        completion(JSON.null, err)
+                        // clearing Network's current request
+                        self.currentRequest = nil
+                    } else {
+                        print("Error: ", error)
+                        completion(JSON.null, error)
+                        // clearing Network's current request
+                        self.currentRequest = nil
+                    }
+                }
+        }
+        // setting Network's current request
+        if let request = self.currentRequest {
+            print(request.description)
+        }
+    }
+    
+}
+
+// MARK: - Request's enum parameters & values
+
+enum Path: String {
+    case signIn = "signin"
+    case signUp = "signup"
+    case logout = "logout"
+    case userDevice = "user/deviceId"
+    case mosaics = "mosaics"
+    case mosaicsList = "mosaicsList"
+    case resetPassword = "forgot_password"
+}
+
+enum APIRequest {
+    case signIn(body: Parameters)
+    case signUp(body: Parameters)
+    case logout(queryID: String)
+    case registerDevice(token: String)
+    case collections(userID: String)
+    case collectionsList()
+    case resetPassword(body: Parameters)
+}
+
+// MARK: - RequestProtocol - URLs extension
+
+extension RequestProtocol {
+    
+    var baseURL: String {
+        return serverURL
+    }
+    
+    var fullURL: String {
+        var url = baseURL + "api/v1/" + path.rawValue
+        if let qid = queryID {
+            url = "\(url)/\(qid)"
+        }
+        return url
+    }
+    
+}
+
+// MARK: - RequestProtocol - APIRequest parameters
+
+extension APIRequest: RequestProtocol {
+    
+    var path: Path {
+        switch self {
+        case .signIn:
+            return .signIn
+        case .signUp:
+            return .signUp
+        case .logout:
+            return .logout
+        case .registerDevice:
+            return .userDevice
+        case .collections:
+            return .mosaics
+        case .collectionsList:
+            return .mosaicsList
+        case .resetPassword:
+            return .resetPassword
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .signIn, .signUp, .resetPassword:
+            return .post
+        case .logout, .registerDevice:
+            return .put
+        default:
+            return .get
+        }
+    }
+    
+    var encoding: ParameterEncoding {
+        switch self {
+        case .signIn, .signUp, .resetPassword:
+            return JSONEncoding.default
+        default:
+            return URLEncoding.default
+        }
+    }
+    
+    var parameters: Parameters {
+        var parameters: Parameters
+        switch self {
+        case .signIn(let body):
+            parameters = body
+        case .signUp(let body):
+            parameters = body
+        case .collections(let userID):
+            parameters = ["userId": userID]
+        case .resetPassword(let body):
+            parameters = body
+        default:
+            parameters = Parameters()
+        }
+        if !parameters.isEmpty {
+            print("parameters: \(parameters)")
+        }
+        return parameters
+    }
+    
+    var queryID: String? {
+        switch self {
+        case .logout(let queryID):
+            return queryID
+        case .registerDevice(let token):
+            return token
+        default:
+            return nil
+        }
+    }
+    
+    var headers: HTTPHeaders {
+        var headers = HTTPHeaders()
+        switch self {
+        case .signIn, .signUp, .resetPassword:
+            break
+        default:
+            if let accessToken = Settings.accessToken() {
+                headers["Authorization"] = "Bearer " + accessToken
+            }
+        }
+        return headers
+    }
+    
+}
+
+// MARK: - Network extesnions
+
+extension Network {
+    
+    // Registering device ID with push token on backend
+    class func registerPushToken() {
+        if let pushToken = Settings.pushToken() {
+            Network.shared.request(.registerDevice(token: pushToken)) { (json, error) in
+                if let err = error {
+                    print(err)
+                    return
+                }
+            }
+        }
+    }
+    
+    // Download function
+    func download(_ url: String, completion: @escaping Completion) {
+        let destination = DownloadRequest.suggestedDownloadDestination(for: .documentDirectory)
+        self.sessionManager.download(url, to: destination)
+            .responseData(completionHandler: { (dataResponse) in
+                switch dataResponse.result {
+                case .success:
+                    completion(nil)
+                case .failure(let error):
+                    print("Error: ", error)
+                    completion(error)
+                }
+            })
+    }
+    
+}
---
+++ RegisterViewController.swift
@@ -0,0 +1,191 @@
+//
+//  RegisterViewController.swift
+//  Memphis
+//
+//  Created by Igor Tudoran on 1/10/19.
+//  Copyright © 2019 Do IT. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class RegisterViewController: BaseViewController {
+
+    @IBOutlet weak var titleConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmField: UITextField!
+    @IBOutlet weak var inviteField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var singInButton: UIButton! {
+        didSet {
+            let attrbs : [NSAttributedString.Key : Any]
+                = [NSAttributedString.Key.font : UIFont.lato(.regular, size: 18.0),
+                   NSAttributedString.Key.foregroundColor : UIColor.white,
+                   NSAttributedString.Key.underlineStyle : NSUnderlineStyle.single.rawValue]
+            let name = NSAttributedString(string: "Sign In", attributes: attrbs)
+            singInButton.setAttributedTitle(name, for: .normal)
+        }
+    }
+    
+    private var defaultConstrainHeight: CGFloat = 40.0
+    private var moveDelta: CGFloat = -36.0
+    private var registerModel: RegisterModel!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        allowHidingKeyboard()
+        
+        // UI fix for SE & 5S iPhones
+        if isPhoneSE {
+            titleConstraint.constant = 18.0
+            defaultConstrainHeight = 18.0
+            titleLabel.font = UIFont.lato(.regular, size: 18)
+        }
+        
+        // Registering observer for keyboard notifications
+        notificationCenter.addObserver(self,
+                                       selector: #selector(animateWithKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillShowNotification,
+                                       object: nil)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(animateWithKeyboard(notification:)),
+                                       name: UIResponder.keyboardWillHideNotification,
+                                       object: nil)
+    }
+    
+    deinit {
+        // Removing keyboards observer
+        notificationCenter.removeObserver(self)
+    }
+    
+    // MARK: - Functions
+    
+    // Animation for keyboard when it shows/hides
+    @objc func animateWithKeyboard(notification: NSNotification) {
+        let moveUp = (notification.name == UIResponder.keyboardWillShowNotification)
+        titleConstraint.constant = moveUp ? moveDelta : defaultConstrainHeight
+        UIView.animate(withDuration: kDefaultAnimationDuration) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    // Checking fields validation
+    private func areFieldsValid() -> Bool {
+        guard let email = emailField.text, email.isValidEmail() else {
+            showAlert("Invalid email", text: "Please check the email and try again")
+            return false
+        }
+        guard let username = usernameField.text, username.isValidCommonField() else {
+            showAlert("Invalid username", text: "Please check the username and try again")
+            return false
+        }
+        guard let password = passwordField.text, password.isValidPassword() else {
+            showAlert("Invalid password", text: "Please check the password and try again")
+            return false
+        }
+        guard let confirmPassword = confirmField.text, confirmPassword.isValidPassword() else {
+            showAlert("Invalid confirm password", text: "Please check the confirm password and try again")
+            return false
+        }
+        guard let inviteCode = inviteField.text, inviteCode.isValidCommonField() else {
+            showAlert("Invalid invite code", text: "Please check the invite code and try again")
+            return false
+        }
+        if password != confirmPassword {
+            showAlert("Passwords do not match", text: "Please check the passwords and try again")
+            return false
+        }
+        if inviteCode != "welcome" {
+            showAlert("Invalid invite code", text: "Please check the invite code and try again")
+            return false
+        }
+        
+        // Saving register model
+        registerModel = RegisterModel()
+        registerModel.email = email
+        registerModel.password = password
+        registerModel.username = username
+        registerModel.inviteCode = inviteCode
+        return true
+    }
+    
+    // Sing un functions with saving user data
+    func signUp() {
+        if !Network.isReachable {
+            showAlert("No internet connection", text: "Please check Network Settings")
+            return
+        }
+        showHUD()
+        print("Sign UP with \(String(describing: registerModel))")
+        let params = ["email": registerModel.email,
+                      "password": registerModel.password,
+                      "name": registerModel.username,
+                      "device_id": Settings.pushToken() ?? deviceID]
+        // Making signup request
+        Network.shared.request(.signUp(body: params)) { (json, error) in
+            self.hideHUD()
+            if let err = error {
+                self.showAlert(text: err.localizedDescription)
+                return
+            }
+            // Return to login with explanation alert
+            self.showAlert("Activate your account", text: "Please check your email for more instructions")
+            AppDelegate.shared.root.switchToLogin()
+        }
+    }
+    
+    // MARK: - IB Actions
+    
+    @IBAction func singUpPressed(_ sender: Any) {
+        if areFieldsValid() {
+            signUp()
+        }
+    }
+    
+    @IBAction func singInPressed(_ sender: Any) {
+        AppDelegate.shared.root.switchToLogin()
+    }
+    
+}
+
+// MARK: - UITextFieldDelegate Delegate
+
+extension RegisterViewController: UITextFieldDelegate {
+    
+    // Done button actions
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField {
+            usernameField.becomeFirstResponder()
+        }
+        if textField == usernameField {
+            passwordField.becomeFirstResponder()
+        }
+        if textField == passwordField {
+            confirmField.becomeFirstResponder()
+        }
+        if textField == confirmField {
+            inviteField.becomeFirstResponder()
+        }
+        if textField == inviteField {
+            textField.resignFirstResponder()
+            if areFieldsValid() {
+                signUp()
+            }
+        }
+        return false
+    }
+    
+    // Trimming spaces from text fields
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text {
+            let trimmedText = text.trimmingCharacters(in: .whitespaces)
+            textField.text = trimmedText
+        }
+    }
+    
+}
